e720ddea48399c770fc724b61ff3105a3c1a4182
vch <ghub@cheche.ro>
2017-03-16 19:41:55 +0200

    add def env to nsa310 nsa310s nsa325

--- include/configs/nsa310s.h
+++ include/configs/nsa310s.h
@@ -77,50 +77,51 @@
  * Default environment variables
  */
 #define CONFIG_BOOTCOMMAND \
-	"run bootcmd_uenv; run bootcmd_usb; usb stop; run bootcmd_sata; reset" 
+	"mw 0x800000 0 1; run set_init_devices; run scan_disk; run bootcmd_uenv; run set_bootargs; run bootcmd_exec; sleep 5; reset"
 
 #define CONFIG_EXTRA_ENV_SETTINGS \
-	"console=console=ttyS0,115200\0" \
 	"arcNumber=4931\0" \
-	"baudrate=115200\0" \
-	"bootcmd_sata=run sata_init; run set_bootargs_sata; run sata_boot\0" \
-	"bootcmd_usb=run usb_init; run set_bootargs_usb; run usb_boot\0" \
-	"bootdelay=10\0" \
-	"console=ttyS0,115200\0" \
-	"device=0:1\0" \
-	"ethact=egiga0\0" \
-	"ethaddr=b6:d0:5e:0f:a1:17\0" \
-	"led_error=orange blinking\0" \
-	"led_exit=green off\0" \
-	"led_init=green blinking\0" \
-	"machid=118f\0" \
-	"mainlineLinux=yes\0" \
-	"mtdids=nand0=orion_nand\0" \
-	"mtdparts=mtdparts=orion_nand:0x100000(uboot),0x80000(stock_uboot_env),0x80000(key_store)," \
-	"0x80000(info),0xA00000(etc),0xA00000(kernel_1),0x2FC0000(rootfs1),0xA00000(kernel_2),0x2FC0000(rootfs2)\0"\
-	"partition=nand0,2\0" \
-	"rootdelay=10\0" \
-	"rootfstype=ext2\0" \
-	"sata_boot=mw 0x800000 0 1; run sata_load_uimage; if run sata_load_uinitrd; then bootm 0x800000 0x1100000; else bootm 0x800000; fi\0" \
-	"sata_init=ide reset\0" \
-	"sata_load_uimage=ext2load ide $device 0x800000 /boot/uImage\0" \
-	"sata_load_uinitrd=ext2load ide $device 0x1100000 /boot/uInitrd\0" \
-	"sata_root=/dev/sda1\0" \
-	"set_bootargs_sata=setenv bootargs console=$console root=$sata_root rootdelay=$rootdelay rootfstype=$rootfstype $mtdparts\0" \
-	"set_bootargs_usb=setenv bootargs console=$console root=$usb_root rootdelay=$rootdelay rootfstype=$rootfstype $mtdparts\0" \
+	"preboot=setenv stderr serial; setenv stdin serial; setenv stdout serial; setenv baudrate 115200; setenv console ttyS0,$baudrate; setenv bootdelay 10\0" \
 	"stderr=serial\0" \
 	"stdin=serial\0" \
 	"stdout=serial\0" \
-	"usb_boot=mw 0x800000 0 1; run usb_load_uimage; if run usb_load_uinitrd; then bootm 0x800000 0x1100000; else bootm 0x800000; fi\0" \
-	"usb_init=usb start\0" \
-	"usb_load_uimage=ext2load usb $device 0x800000 /boot/uImage\0" \
-	"usb_load_uinitrd=ext2load usb $device 0x1100000 /boot/uInitrd\0" \
-	"usb_root=/dev/sda1\0" \
-	"bootcmd_uenv=run uenv_load; if test $uenv_loaded -eq 1; then run uenv_import; fi\0" \
-	"uenv_import=echo importing envs ...; env import -t 0x810000\0" \
-	"uenv_load=usb start; setenv uenv_loaded 0; for devtype in usb; do for disknum in 0; do run uenv_read_disk; done; done\0" \
-	"uenv_read=echo loading envs from $devtype $disknum ...; if load $devtype $disknum:1 0x810000 /boot/uEnv.txt; then setenv uenv_loaded 1; fi\0" \
-	"uenv_read_disk=if $devtype part $disknum; then run uenv_read; fi"
+	"baudrate=115200\0" \
+	"console=ttyS0,$baudrate\0" \
+	"bootdelay=10\0" \
+	"led_error=orange blinking\0" \
+	"led_exit=green off\0" \
+	"led_init=green blinking\0" \
+	"ethact=egiga0\0" \
+	"ethaddr=00:19:cb:00:51:81\0" \
+	"mtdids=nand0=orion_nand\0" \
+	"mtdparts=mtdparts=orion_nand:0x100000(uboot),0x80000(stock_uboot_env),0x80000(key_store),0x80000(info),0xA00000(etc),0xA00000(kernel_1),0x2FC0000(rootfs1),0xA00000(kernel_2),0x2FC0000(rootfs2)\0" \
+	"partition=nand0,2\0" \
+	"file_uimage=/boot/uImage\0" \
+	"file_uinitrd=/boot/uInitrd\0" \
+	"file_uenv=/boot/uEnv\0" \
+	"addr_uimage=0x0800000\0" \
+	"addr_uenv=0x0810000\0" \
+	"addr_uinitrd=0x1100000\0" \
+	"addr_dtb=0x1c00000\0" \
+	"devices=usb ide\0" \
+	"disks=0 1 2 3\0" \
+	"device=0:1\0" \
+	"bootdev=usb\0" \
+	"usb_ready_retry=15\0" \
+	"rootdelay=10\0" \
+	"uenv_loaded=0\0" \
+	"scan_done=0\0" \
+	"set_init_devices=setenv init_usb \"usb start\"; setenv init_ide \"ide reset\"\0" \
+	"flash_new_uboot=run set_init_devices; run init_usb; sleep 5; if load usb 0:1 $addr_uimage uboot.img; then nand erase 0x0 0xc0000; nand write $addr_uimage 0x0 0x80000; reset; fi\0" \
+	"flash_new_uenv=run set_init_devices; run init_usb; sleep 5; if load usb 0:1 $addr_uimage uenv000000000000.img; then nand erase 0xc0000 0x20000; nand write $addr_uimage 0xc0000 0x20000; reset;	fi\0" \
+	"load_dtb=echo \"LOADING DTB $file_dtb ...\"; load $bootdev $device $addr_dtb $file_dtb; echo \"_________________________________________________\"\0" \
+	"load_uimage=echo \"LOADING uImage $file_uimage ...\"; load $bootdev $device $addr_uimage $file_uimage; echo \"_________________________________________________\"\0" \
+	"load_uinitrd=echo \"LOADING uInitrd $file_uinitrd ...\" ; load $bootdev $device $addr_uinitrd $file_uinitrd; echo \"_________________________________________________\"\0" \
+	"set_bootargs=setenv bootargs console=$console root=$rootfs rootdelay=$rootdelay rootfstype=$rootfstype $mtdparts $initsystem $custom_params\0" \
+	"scan_disk=echo \"_________________________________________________\"; echo \"  SCANNING FOR BOOT DRIVE...\"; echo \"  BOOT PRIORITY: $devices \"; run set_init_devices; for dev in $devices; do if test $scan_done -eq 0; then echo \"_________________________________________________\"; echo \"  SCAN DEVICE $dev\"; run init_$dev; for disknum in $disks; do if test $scan_done -eq 0; then echo \"_________________________________________________\"; echo \"  DEVICE $dev $disknum:1\"; if load $dev $disknum:1 $addr_uimage $file_uimage 1; then setenv scan_done 1; echo \"  FOUND BOOTABLE DRIVE ON $dev $disknum\"; echo \"_________________________________________________\"; setenv device $disknum:1; setenv bootdev $dev; fi; fi; done; fi; done; sleep 3\0" \
+	"bootcmd_exec=run load_uimage; if run load_uinitrd; then if run load_dtb; then bootm $addr_uimage $addr_uinitrd $addr_dtb; else bootm $addr_uimage $addr_uinitrd; fi; else if run load_dtb; then bootm $addr_uimage - $addr_dtb; else bootm $addr_uimage; fi; fi\0" \
+	"bootcmd_uenv=echo \"_________________________________________________\"; echo \"  LOOKING FOR EXTRA ENV FILE: $file_uenv\"; echo \"  ON BOOT PARTITION: $bootdev $device \"; echo \"                                                 \"; if load $bootdev $device $addr_uenv $file_uenv; then setenv uenv_loaded 1; echo \"                                                 \"; echo \"  LOADED EXTRA ENVS FROM $bootdev $device ... \"; echo \"                                                 \"; fi; if test $uenv_loaded -eq 1; then echo \"  IMPORTING EXTRA ENVS...\"; echo \"_________________________________________________\"; env import -t $addr_uenv $filesize; fi; sleep 3"
+
 
 /*
  * Ethernet Driver configuration
