360ef663ca87f42925531dc916bdaf5eec1052ef
vch <ghub@cheche.ro>
2017-03-17 00:16:38 +0200

    nsa320 change nand env offset to 0x00100000 (stock location)

--- include/configs/nsa320.h
+++ include/configs/nsa320.h
@@ -91,8 +91,8 @@
  * it has to be rounded to sector size
  */
 #define CONFIG_ENV_SIZE			0x20000		/* 128k */
-#define CONFIG_ENV_ADDR			0xc0000
-#define CONFIG_ENV_OFFSET		0xc0000	/* env starts here */
+#define CONFIG_ENV_ADDR			0x100000
+#define CONFIG_ENV_OFFSET		0x100000	/* env starts here */
 
 /*
  * Default environment variables
